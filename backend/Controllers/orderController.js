6482d933e534f2c7f1f201724e5ebe62b22cef73
Divya Farswan <divyaf01@gmail.com>
2025-08-19 21:29:26 +0530

    changes in order controller

--- backend/Controllers/orderController.js
+++ backend/Controllers/orderController.js
@@ -1,12 +1,6 @@
-import dotenv from "dotenv";
-dotenv.config();
 import orderModel from "../Models/orderModel.js";
 import userModel from './../Models/userModel.js';
-import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2024-06-20',           
-});
 
 //placing user order for frontend
 const placeOrder = async (req, res) => {
@@ -43,16 +37,6 @@ const placeOrder = async (req, res) => {
             },
             quantity: 1
         })
-
-        const session = await stripe.checkout.sessions.create({
-            line_items: line_items,
-            mode: 'payment',
-            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
-        })
-
-        res.json({ success: true, session_url: session.url })
-
     } catch (error) {
         console.log(error)
         res.json({ success: false, message: "Error" })

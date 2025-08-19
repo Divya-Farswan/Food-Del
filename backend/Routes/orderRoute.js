2bb63692b6609802c0d36ba97fbf40f97cfe2945
Divya Farswan <divyaf01@gmail.com>
2025-08-19 21:50:47 +0530

    changes in order controller

--- backend/Routes/orderRoute.js
+++ backend/Routes/orderRoute.js
@@ -1,13 +1,14 @@
 import express from 'express'
-import  { listOrders, placeOrder, updateStatus, userOrders, verifyOrder } from '../Controllers/orderController.js';
+// import  { listOrders, placeOrder, updateStatus, userOrders, verifyOrder } from '../Controllers/orderController.js';
 import cartAuth from '../Middleware/cartAuth.js';
+import { placeOrder } from '../Controllers/orderController.js';
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place", cartAuth, placeOrder)
-orderRouter.post("/verify", verifyOrder)
-orderRouter.post("/usersorders", cartAuth, userOrders)
-orderRouter.get("/list", listOrders)
-orderRouter.post("/status", updateStatus)
+// orderRouter.post("/verify", verifyOrder)
+// orderRouter.post("/usersorders", cartAuth, userOrders)
+// orderRouter.get("/list", listOrders)
+// orderRouter.post("/status", updateStatus)
 
 export default orderRouter;
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 import foodRouter from './Routes/foodRoute.js';
 import userRouter from './Routes/userRoute.js';
 import cartRouter from './Routes/cartRoute.js';
-import { verifyOrder } from './Controllers/orderController.js';
+// import { verifyOrder } from './Controllers/orderController.js';
 import orderRouter from './Routes/orderRoute.js';
 import connectCloudinary from './Config/cloudinaryConfig.js';
 import connectDB from './Config/dbConfig.js';
@@ -24,7 +24,7 @@ app.use("/api/food", foodRouter);
 app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
 app.use("/api/order", orderRouter)
-app.use("/api/verify", verifyOrder)
+// app.use("/api/verify", verifyOrder)
 
 app.get("/", (req, res) => {
     res.send("API working...")

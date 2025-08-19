d3cb9453f32eeb4c8cce34926a1407f2a732e49d
Divya Farswan <divyaf01@gmail.com>
2025-08-19 15:30:25 +0530

    Changes mad in food Route

--- backend/Routes/foodRoute.js
+++ backend/Routes/foodRoute.js
@@ -1,12 +1,10 @@
-// routes/foodRouter.js
 import express from "express";
-import { addFood, listFood, removeFood } from "../controllers/foodController.js";
+import { addFood, listFood, removeFood } from "../Controllers/foodController.js";
 import upload from "../Middleware/multer.js";
 import adminAuth from './../Middleware/adminAuth.js';
 
 const foodRouter = express.Router();
 
-// POST - accepts multipart/form-data with field "image"
 foodRouter.post("/add",adminAuth, upload.single("image") , addFood);
 foodRouter.get("/list", listFood);
 foodRouter.post("/remove",adminAuth, removeFood);
